284fe014d4340fe1693f21490318f71dfdc57e3e
cadenhong <caden.p.hong@gmail.com>
2023-03-21 11:52:20 -0400

    regex 2nd mod quizzes

---
+++ 3-regular-expressions/2-basic-regular-expressions/quiz/q2.py
@@ -0,0 +1,14 @@
+'''
+The check_time function checks for the time format of a 12-hour clock, as follows: the hour is between 1 and 12, with no leading zero, followed by a colon, then minutes between 00 and 59, then an optional space, and then AM or PM, in upper or lower case.
+'''
+
+import re
+def check_time(text):
+  pattern = r"^(1[0-2]|[1-9]):[0-5][0-9]\s?(am|pm)$"
+  result = re.search(pattern, text, re.IGNORECASE)
+  return result != None
+
+print(check_time("12:45pm")) # True
+print(check_time("9:59 AM")) # True
+print(check_time("6:60am")) # False
+print(check_time("five o'clock")) # False
